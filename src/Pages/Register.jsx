a8de634d5edb01cd80b24478c2ed63460cb409eb
anuragtj <anuragwadhwa786@gmail.com>
2024-07-02 19:33:19 +0530

    feat: register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,5 +1,72 @@
+import "../Styles/Register.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Select from "react-select";
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [gender, setGender] = useState(null);
+
+  const genderOptions = [
+    { value: "male", label: "Male" },
+    { value: "female", label: "Female" },
+    { value: "other", label: "Other" },
+  ];
+  return (
+    <div className="registerContainer">
+      <h1>Sign up to Shri Nath Ji Jewellers</h1>
+      <div className="nameGenderContainer">
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          className="inputField"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <Select
+          options={genderOptions}
+          value={gender}
+          onChange={(selectedOption) => setGender(selectedOption)}
+          placeholder="Select Gender"
+          className="selectField"
+        />{" "}
+      </div>
+      <div>
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          className="inputField email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          className="inputField email"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+
+      <div className="buttonContainer">
+        <button type="submit" className="submitButton">
+          Sign up
+        </button>
+        <p className="signupLink">
+          Already have an account?{" "}
+          <Link to="/register" className="highlightLink">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
